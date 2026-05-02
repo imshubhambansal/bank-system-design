84ad70730daa8fc72e44c30cce38c801496639ac
Shubham <shubhambansal3096@gmail.com>
2026-05-02 21:31:08 +0530

    abstract class Account

--- model/Account.java
+++ model/Account.java
@@ -1,5 +1,46 @@
+// Abstract Class Account
+
 package model;
 
-public class Account {
+import java.util.*;
+import java.lang.IllegalArgumentException;
+import exception.InsufficientBalanceException;
+
+public abstract class Account {
+    protected String accountNumber;
+    protected double balance;
+    protected List<Transaction> transactions;
+
+    // Parameterized Constructor
+    public Account(String accountNumber){
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+        this.transactions = new ArrayList<>();
+    }
+
+    // Deposit Method
+    public void deposit(double amount){
+        
+        // Amount validity check
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Invalid Deposit Amount");
+        }
+        
+        // Balance update
+        balance += amount;
+
+        // Transaction Creation
+        Transaction transaction = new Transaction(UUID.randomUUID().toString(), "DEPOSIT", amount);
+
+        // Transaction added to history
+        transactions.add(transaction);
+    }
+
+    // Withdraw Method
+    public abstract void withdraw(double amount) throws InsufficientBalanceException;
     
+    // Transaction history Method
+    public List<Transaction> geTransactions() {
+        return transactions;
+    }
 }
